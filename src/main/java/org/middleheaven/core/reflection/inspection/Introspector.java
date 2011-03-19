c06390a9ffa5c4e0bf6343a5ef985efea2ec9594
sergio.m.taborda <sergio.m.taborda@ea7a7daa-3a4e-0410-9179-c7af98cb395f>
2011-03-19 18:33:09 +0000

    Inspection Revision

---
+++ src/main/java/org/middleheaven/core/reflection/inspection/Introspector.java
@@ -0,0 +1,41 @@
+package org.middleheaven.core.reflection.inspection;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+
+
+/**
+ * High level abstraction for handling types metadata.
+ */
+public abstract class Introspector {
+
+	public static <T> ObjectInstrospector<T> of(T object){
+		return new ObjectInstrospector<T>(object);
+	}
+	
+
+	public static <T> ClassIntrospector<T> of(Class<T> type){
+		return new ClassIntrospector<T>(type);
+	}
+
+	public static PackageIntrospector of(Package typePackage) {
+		return typePackage == null ? null : new PackageIntrospector(typePackage);
+	}
+	
+	public static MethodIntrospector of(Method method) {
+		return method == null ? null : new MethodIntrospector(method);
+	}
+	
+	public static FieldIntrospector of(Field field) {
+		return field == null ? null : new FieldIntrospector(field);
+	}
+	
+	// package
+	Introspector(){}
+	
+	public abstract <A extends Annotation> boolean isAnnotadedWith(Class<A> annotationClass);
+
+	public abstract <A extends Annotation> A getAnnotation(Class<A> annotationClass);
+}
