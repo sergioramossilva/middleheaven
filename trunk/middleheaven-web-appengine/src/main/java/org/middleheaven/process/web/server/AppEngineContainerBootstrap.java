a1c0972a5828633a5688b546016c4d71fd127b2c
sergio.m.taborda <sergio.m.taborda@ea7a7daa-3a4e-0410-9179-c7af98cb395f>
2012-02-29 02:56:39 +0000

    implementatio of close for filewatchservice and filewatchchannelprocessor

--- trunk/middleheaven-web-appengine/src/main/java/org/middleheaven/process/web/server/AppEngineContainerBootstrap.java
+++ trunk/middleheaven-web-appengine/src/main/java/org/middleheaven/process/web/server/AppEngineContainerBootstrap.java
@@ -4,7 +4,6 @@
 package org.middleheaven.process.web.server;
 
 import org.middleheaven.core.bootstrap.BootstrapContainer;
-import org.middleheaven.io.repository.ManagedFile;
 import org.middleheaven.web.container.WebContainer;
 import org.middleheaven.web.container.WebContainerInfo;
 
@@ -13,18 +12,9 @@ import org.middleheaven.web.container.WebContainerInfo;
  */
 public class AppEngineContainerBootstrap extends AbstractWebContainerBootstrap {
 
-	/**
-	 * {@inheritDoc}
-	 */
 	@Override
-	protected ManagedFile getEnvironmentRootFolder() {
-		// TODO uso blob api
-		return null;
-	}
-
-	@Override
-	public final BootstrapContainer resolveContainer(ManagedFile rootFolder) {
-		WebContainer container =  new AppEngineWebContainer(this.servletContext , rootFolder);
+	public final BootstrapContainer resolveContainer() {
+		WebContainer container =  new AppEngineWebContainer(this.servletContext);
 			
 		this.servletContext.setAttribute(WebContainerInfo.ATTRIBUTE_NAME, container.getWebContainerInfo());
 		
